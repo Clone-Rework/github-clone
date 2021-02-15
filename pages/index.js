a4c6a706bbe856f95fa7b70f97b27d536dcbef15
TolgaKara <tolgakara2.0@gmail.com>
2021-02-15 22:21:53 +0100

    finished RepoNav

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,6 @@
 import Head from "next/head";
 import Navbar from "./components/Navbar";
+import { RepoNav } from "./components/RepoNav";
 import { RepoTabs } from "./components/RepoTabs";
 
 export default function Home() {
@@ -7,6 +8,7 @@ export default function Home() {
 		<div className='text-gray-900 text-sm'>
 			<Navbar />
 			<RepoTabs />
+			<RepoNav />
 		</div>
 	);
 }
