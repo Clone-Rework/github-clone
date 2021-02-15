6eb1a1b0e799862207c3883c2fc5134bd3b01c9d
TolgaKara <tolgakara2.0@gmail.com>
2021-02-15 22:05:56 +0100

    Completed Repo Tabs

--- pages/index.js
+++ pages/index.js
@@ -1,10 +1,12 @@
 import Head from "next/head";
 import Navbar from "./components/Navbar";
+import { RepoTabs } from "./components/RepoTabs";
 
 export default function Home() {
 	return (
 		<div className='text-gray-900 text-sm'>
 			<Navbar />
+			<RepoTabs />
 		</div>
 	);
 }
